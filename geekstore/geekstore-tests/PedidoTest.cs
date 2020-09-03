b9d1177c49eba0fcc99c38327c3d68809edf17d8
Alan Vieira Martins <alanhorcrux@gmail.com>
2020-09-02 21:29:30 -0300

    Adicionado FluenteValidation e Teste de Pedidos

---
+++ geekstore/geekstore-tests/PedidoTest.cs
@@ -0,0 +1,23 @@
+﻿using geekstore.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace geekstore_tests
+{
+    public class PedidoTest
+    {
+        [Fact(DisplayName = "Pedido")]
+        [Trait("Categoria", "Pedido Tests")]
+        public void CriarPedido_AdicionarCliente_NaoPodeSerVazioOuNulo()
+        {
+            // Arrange  Act
+            var pedido = new Pedido(1, DateTime.Now, 1);
+
+            // Assert
+            Assert.False(String.IsNullOrEmpty(pedido.IdCliente.ToString()));
+
+        }
+    }
+}
